0548fe0831d24e35199d692cc3dd29d79f409e51
Dharaneeshhh <126551377+Dharaneeshhh@users.noreply.github.com>
2024-03-11 14:44:32 +0530

    Add files via upload

---
+++ final/backend/src/main/java/com/chess/pk/dto/response/CourseResponse.java
@@ -0,0 +1,21 @@
+package com.chess.pk.dto.response;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CourseResponse {
+    private Long courseId;
+    
+    private String courseName;
+    private String content;
+    private String message;
+}
+
+
